c7fb5da3700fb37c470b8fda7c17cbcac507f2f5
Saeed Zakeri <saeed.zakeri.dev@gmail.com>
2024-07-20 19:35:22 +0330

    Add files

---
+++ attributeBinding/src/app/switchcase3/switchcase3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-switchcase3',
+  templateUrl: './switchcase3.component.html',
+  styleUrls: ['./switchcase3.component.css']
+})
+export class Switchcase3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
